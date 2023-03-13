abd02fe57cd8350412cd25e660ecf1e67b08813a
Pedro Dias <pedro@nex.digital>
2023-03-13 14:11:23 -0300

    update texts

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ package zoo;
 public class Main {
 
 	public static void main(String[] args) {
-		Ave pombo = new Ave("birco", 12, "passaro", true, "bico 1");
-		Mamifero gato = new Mamifero("savro", 12, "felino", true, 2);
-		Reptil jacare = new Reptil("care", 12, "sei la", true, 15);
+		Ave pombo = new Ave("Bird", 12, "Passaro", true, "bico 1");
+		Mamifero gato = new Mamifero("Cat", 12, "Felino", true, 2);
+		Reptil jacare = new Reptil("Croc", 12, "Jacare", true, 15);
 		
 		Zoologico zoo = new Zoologico();
 		
